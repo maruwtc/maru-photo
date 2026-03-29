b41ab99f8849ab5eef3bc806334d90cbd7717059
maruwtc <chriswong0621@gmail.com>
2026-03-29 21:42:28 +0800

    fix: update development and start script flags in package.json for proper host binding

--- auth-server/frontend/next.config.ts
+++ auth-server/frontend/next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
     ],
+  },
+  turbopack: {
+    root: path.join(__dirname, '../../'), // Adjust the path as needed to point to the correct root
   },
 };
 
